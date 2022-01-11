57b9d231a76594f22162d723bea9ead18bbdd533
Vlad <kivaka-mega@yandex.ru>
2022-01-11 20:43:31 +0300

    add ki

---
+++ 2 раздел.py
@@ -0,0 +1,52 @@
+#1
+# namberX=int(input("enter x"))
+# msg1=f"17*{namberX**2}-6*{namberX}+13={17 * namberX ** 2 - 6 * namberX + 13}"
+# print(msg1)
+
+#2
+# namberX=int(input("enter x"))
+# msg1=f"3*{namberX**2}-5*{namberX}-21={3 * namberX ** 2 - 5 * namberX - 21}"
+# print(msg1)
+
+#2.2
+# namberA=int(input("enter a"))
+# msg=f"{namberA**2}+10/(({(namberA**2)}+1)**0,5)={(namberA**2+10)/(((namberA**2)+1)**0.5)}"
+# print(msg)
+
+#3
+# import math
+# numberA=int(input(""))
+# msg=f"(2{numberA}+sin|3*{numberA}|/3.56)**0.5={((2*numberA+math.sin(abs(3*numberA)))/3.56)**0.5}"
+# print(msg)
+
+#3.2
+# import math
+# numberA=int(input(""))
+# msg=f"sin(3.2 + ((1+{numberA })*0.5)/|5*{numberA }|)={math.sin((3.2+(1+numberA)*0.5)/(abs(5*numberA)))}"
+# print(msg)
+
+#4
+# a=5
+# p=5*4
+# print("perimetr=",p)
+
+#5
+# r=5
+# d=r*2
+# print("diametr=",d)
+
+#6
+
+
+#7
+# a=5
+# s=a*a*6
+# v=a**3
+# print(s,v)
+
+#8
+import math
+# r=5
+# p=2*math.pi*r
+# s=2*math.pi*(r**2)
+# print(p,s)
