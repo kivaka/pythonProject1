d1a6e1f7a106878698303fd0f7fa69d9ff2485ca
Vlad <kivaka-mega@yandex.ru>
2022-01-11 20:42:05 +0300

    add ki

---
+++ 1_раздел.py
@@ -0,0 +1,73 @@
+# # 1
+# # print(31, end=" ")
+# # print(18, end=" ")
+# # print(79)
+#
+# # 2
+# # print(31, end="  ")
+# # print(18, end="  ")
+# # print(79)
+#
+# # 3
+# # print(50)
+# # print(150)
+#
+# # 4
+# # msg=f"5\n10\n21"
+# # print(msg)
+#
+# #5
+# # msg=f"1\n2"
+# # print(msg)
+#
+# # #6
+# # import math
+# # print("%.3f"%math.pi)
+#
+# #7
+# # import math
+# # print("%.1f"%math.e)
+#
+# #8
+# # znachen=int(input())
+# # print("vi velli cheslo", znachen )
+#
+# # 9
+# # znachen=int(input())
+# # print("vi velli cheslo", znachen )
+#
+# # 9
+# # znachen=int(input())
+# # print("vi velli cheslo", znachen )
+#
+# # 11
+# # name=input("enter you name? ")
+# # print("you name", name)
+#
+# # 12
+# name=input()
+# print("hello, ",name,"!")
+
+#13
+# number=int(input())
+# msg=f"following the number {number} number {number+1}"
+# msg2=f"for the {number} preceding number  {number-1}"
+# print(msg,"\n",msg2)
+
+# 14
+#
+# number1, number2, number3=(int(input(  )) for _ in range (3))
+# print(number1 ,"  ", number2,"  ", number3)
+
+#15
+# number1, number2, number3, number4=(int(input(  )) for _ in range (4))
+# print(number1 ," ", number2," ", number3," ", number4)
+
+# 16
+# msg=f"a) 5 10\t б) 100 t\t в) x 25\n   7 см\t    1949 v\t    x y "
+# print(msg)
+
+# 17
+# a,b,x,y=(int(input())for i in range (4))
+# msg=f"a) 2 кг\t б) {a} 1\t в) {x} {y}\n   13 17\t19 {b}\t5 {y} "
+# print(msg)
